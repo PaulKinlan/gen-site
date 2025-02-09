448f5b883a63e4cba04317615fb027fc39368393
Paul Kinlan <paul.kinlan@gmail.com>
2025-02-09 00:52:00 +0000

    Different Cache key for custom domain

--- routes/decorators/cache.ts
+++ routes/decorators/cache.ts
@@ -2,6 +2,9 @@ import { Cache } from "../../cache.ts";
 import { getContentType } from "../../utils/contentType.ts";
 import { getSiteFromHostname } from "../../utils/hostname.ts";
 import { isMediaFile } from "../../utils/contentType.ts";
+import { db } from "../../db.ts";
+
+const ENV_SaasDomainsAuthToken = Deno.env.get("SAAS_DOMAINS_AUTH_TOKEN");
 
 export function cache({ cache }: { cache: Cache }) {
   return function (
@@ -14,18 +17,30 @@ export function cache({ cache }: { cache: Cache }) {
     descriptor.value = async function (...args: any[]) {
       const [req] = args;
       const url = new URL(req.url);
-      const hostname = url.hostname;
-      const subdomain = getSiteFromHostname(hostname);
       const path = url.pathname;
       const contentType = getContentType(path);
+      const hostname = url.hostname;
+      let subdomain = getSiteFromHostname(hostname);
 
       if (subdomain === undefined) {
         return new Response("Subdomain not found", { status: 404 });
       }
 
-      if (isMediaFile(url)) {
+      if (isMediaFile(url.toString())) {
         // Don't attempt to find image files in the cache.
         return originalMethod.apply(this, args);
+      }
+
+      const SaasDomainsAuthToken = req.headers.get("X-SaaS-Domains-Auth-Token");
+
+      const servedForHeader = req.headers.get("X-Served-For");
+      if (servedForHeader && ENV_SaasDomainsAuthToken == SaasDomainsAuthToken) {
+        // If served through custom domain proxy then we need to change the cache key to the custom domain
+        const site = await db.getSiteByDomain(servedForHeader);
+        // We shouldn't be really looking in the DB here.... but there is no way to know the context yet.
+        if (site) {
+          subdomain = servedForHeader;
+        }
       }
 
       // Skip cache for localhost
